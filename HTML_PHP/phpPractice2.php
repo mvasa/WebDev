b693801ed11504636bb00673a7a22d966ed23a4f
mvasa <31524613+mvasa@users.noreply.github.com>
2017-12-05 22:48:43 -0700

    new folder: javascript

---
+++ HTML_PHP/phpPractice2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php and html</title>
+</head>
+<body>
+<h1>this document: php practice</h1>
+<p>testing out variables in php</p>
+<?php
+echo "Hello World!";
+?>
+</body>
+</html>
